4dfa6d029e8afd7cb16a2bad3ed177a4107e3e96
Záruba Jan <jan.zaruba@dek-cz.com>
2023-01-26 10:34:35 +0100

    fixup interface

--- src/IComgate.php
+++ src/IComgate.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 
 namespace DekApps\Comgate;
 
+use DekApps\Comgate\Model\MethodItemContainer;
+
 interface IComgate
 {
     public function getName(): string;
     public function isMethodsSet(): bool;
     public function getMethodTemplate(): string;
+    public function getMethods(): MethodItemContainer;
+    public function setMethods(MethodItemContainer $methods);
+    public function getDesign(): string;
+    public function setDesign(string $design);
+    public function getShowTitle(): bool;
+    public function getShowDescription(): bool;
+    public function setShowTitle(bool $showTitle);
+    public function setShowDescription(bool $showDescription);
 }
 
 
